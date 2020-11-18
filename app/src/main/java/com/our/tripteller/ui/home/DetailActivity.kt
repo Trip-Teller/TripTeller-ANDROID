eb292cd838bf1e77fb6f5877479adc18dce4ce21
JiHyeonMon <jhk8806@naver.com>
2020-11-19 00:52:53 +0900

    [DetailActivity] 사진 추가, 글 추가 - 작업 중

--- app/src/main/java/com/our/tripteller/ui/home/DetailActivity.kt
+++ app/src/main/java/com/our/tripteller/ui/home/DetailActivity.kt
@@ -2,10 +2,15 @@ package com.our.tripteller.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.our.tripteller.R
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(false)
     }
 }
--- app/src/main/java/com/our/tripteller/ui/home/MainFragment.kt
+++ app/src/main/java/com/our/tripteller/ui/home/MainFragment.kt
@@ -1,11 +1,13 @@
 package com.our.tripteller.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.our.tripteller.R
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : Fragment() {
 
@@ -15,5 +17,14 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_detail.setOnClickListener {
+            val intent = Intent(context, DetailActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
