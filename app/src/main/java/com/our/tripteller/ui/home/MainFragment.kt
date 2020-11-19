255942f60883bceec93cd394b605f3ecdee5f428
Yunhyunjo <gotqlc147@likelion.org>
2020-11-19 11:59:51 +0900

    main ui 수정

--- app/src/main/java/com/our/tripteller/ui/home/MainFragment.kt
+++ app/src/main/java/com/our/tripteller/ui/home/MainFragment.kt
@@ -42,7 +42,7 @@ class MainFragment : Fragment() {
 //            startActivity(intent)
         }
         rv_home.adapter = homeAdapter
-        rv_home.offscreenPageLimit = 2
+        rv_home.offscreenPageLimit = 3
         rv_home.clipToPadding = false
         rv_home.clipChildren = false
         rv_home.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
@@ -78,11 +78,11 @@ class MainFragment : Fragment() {
 
     private fun loadHomeDatas() {
         homeData.apply {
-            add (HomeData(mainimg = "https://images.unsplash.com/photo-1525183995014-bd94c0750cd5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80", region = "부산", intro = "해운대 24년 거주 부산 토박이의\n맛집 여행기 들어보실래요?", tag = "#커플여행"))
-            add (HomeData(mainimg = "https://images.unsplash.com/photo-1571645639045-a3d43f4cafc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=658&q=80", region = "제주도", intro = "해운대 24년 거주 부산 토박이의\n맛집 여행기 들어보실래요?", tag = "#커플여행"))
-            add (HomeData(mainimg = "https://images.unsplash.com/photo-1507041957456-9c397ce39c97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80", region = "강원도", intro = "해운대 24년 거주 부산 토박이의\n맛집 여행기 들어보실래요?", tag = "#커플여행"))
-            add (HomeData(mainimg = "https://images.unsplash.com/photo-1489549132488-d00b7eee80f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80", region = "서울", intro = "해운대 24년 거주 부산 토박이의\n맛집 여행기 들어보실래요?", tag = "#커플여행"))
-            add (HomeData(mainimg = "https://images.unsplash.com/photo-1549194400-06e6874c2fd1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80", region = "부산", intro = "해운대 24년 거주 부산 토박이의\n맛집 여행기 들어보실래요?", tag = "#커플여행"))
+            add (HomeData(mainimg = "https://images.unsplash.com/photo-1525183995014-bd94c0750cd5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80", region = "부산", intro = "해운대 24년 거주 부산 토박이의\n맛집 여행기 들어보실래요?", tag = "커플여행"))
+            add (HomeData(mainimg = "https://images.unsplash.com/photo-1571645639045-a3d43f4cafc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=658&q=80", region = "제주도", intro = "해운대 24년 거주 부산 토박이의\n맛집 여행기 들어보실래요?", tag = "커플여행"))
+            add (HomeData(mainimg = "https://images.unsplash.com/photo-1507041957456-9c397ce39c97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80", region = "강원도", intro = "해운대 24년 거주 부산 토박이의\n맛집 여행기 들어보실래요?", tag = "커플여행"))
+            add (HomeData(mainimg = "https://images.unsplash.com/photo-1489549132488-d00b7eee80f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80", region = "서울", intro = "해운대 24년 거주 부산 토박이의\n맛집 여행기 들어보실래요?", tag = "커플여행"))
+            add (HomeData(mainimg = "https://images.unsplash.com/photo-1549194400-06e6874c2fd1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80", region = "부산", intro = "해운대 24년 거주 부산 토박이의\n맛집 여행기 들어보실래요?", tag = "커플여행"))
         }
         homeAdapter.datas = homeData
         homeAdapter.notifyDataSetChanged()
