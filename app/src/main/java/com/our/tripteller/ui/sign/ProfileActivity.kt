a77003ac69fde210bfff94b6004f1d542802fc68
JiHyeonMon <jhk8806@naver.com>
2020-12-10 19:11:03 +0900

    [ 회원가입 ] Profile - Date Picker 만들기 완료

--- app/src/main/java/com/our/tripteller/ui/sign/ProfileActivity.kt
+++ app/src/main/java/com/our/tripteller/ui/sign/ProfileActivity.kt
@@ -1,16 +1,13 @@
 package com.our.tripteller.ui.sign
 
-import android.R.id.edit
 import android.app.Activity
+import android.app.Instrumentation
 import android.content.Intent
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.our.tripteller.MainActivity
@@ -69,32 +66,10 @@ class ProfileActivity : AppCompatActivity() {
 
 
         //나이 설정
-        val spinner: Spinner = findViewById(R.id.act_profile_spinner_age)
-
-        val adapter = ArrayAdapter.createFromResource(
-            this,
-            R.array.age,
-            android.R.layout.simple_spinner_item
-        )
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
-        spinner.adapter = adapter
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                //
-            }
-
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                spinner.onItemSelectedListener = this
-
-                if (spinner.selectedItem.toString() != "선택") {
-                    spinner.setBackgroundResource(R.drawable.iceblue_roundbox_24)
-                    age = true
-                }
-            }
-
+        act_profile_const.setOnClickListener {
+            val intent = Intent(this, DatePickerActivity::class.java)
+            startActivityForResult(intent, 200)
         }
-
 
         //성별 설정 - 여자
         act_profile_btn_women.setOnClickListener {
@@ -136,8 +111,10 @@ class ProfileActivity : AppCompatActivity() {
                 true
             }
 
-            if (act_profile_spinner_age.selectedItem.toString() == "선택") {
-                act_profile_spinner_age.setBackgroundResource(R.drawable.raspberry_stroke_24)
+            if (act_profile_text_year.text == "") {
+                act_profile_text_year.setBackgroundResource(R.drawable.raspberry_stroke_24)
+                act_profile_text_month.setBackgroundResource(R.drawable.raspberry_stroke_24)
+                act_profile_text_day.setBackgroundResource(R.drawable.raspberry_stroke_24)
                 age = false
             }
 
@@ -153,6 +130,7 @@ class ProfileActivity : AppCompatActivity() {
                 finish()
             }
         }
+
     }
 
 
@@ -182,5 +160,18 @@ class ProfileActivity : AppCompatActivity() {
 
             }
         }
+
+        if (requestCode == 200 && resultCode == 777){
+            act_profile_text_year.text = data?.getIntExtra("year", 0).toString()
+            act_profile_text_month.text = data?.getIntExtra("month", 0).toString()
+            act_profile_text_day.text = data?.getIntExtra("day", 0).toString()
+
+            act_profile_text_year.setBackgroundResource(R.drawable.iceblue_roundbox_24)
+            act_profile_text_month.setBackgroundResource(R.drawable.iceblue_roundbox_24)
+            act_profile_text_day.setBackgroundResource(R.drawable.iceblue_roundbox_24)
+
+            age = true
+        }
     }
+
 }
