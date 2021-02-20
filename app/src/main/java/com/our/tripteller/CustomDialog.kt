c28d42daec9e8f52a81428ea916e00fc877e80c5
Yunhyunjo <gotqlc147@likelion.org>
2021-02-21 02:31:30 +0900

    popup, statusbar

--- app/src/main/java/com/our/tripteller/CustomDialog.kt
+++ app/src/main/java/com/our/tripteller/CustomDialog.kt
@@ -16,18 +16,12 @@ class CustomDialog(context : Context){
     private lateinit var btnCancel : Button
     private lateinit var listener : MyDialogOKClickedListener
 
-    fun start(img: Int, content: String, okText: String, cancelText: String) {
+    fun start(content: String, okText: String, cancelText: String) {
         dlg.requestWindowFeature(Window.FEATURE_NO_TITLE)   //타이틀바 제거
         dlg.setContentView(R.layout.popup_custom)     //다이얼로그에 사용할 xml 파일을 불러옴
         dlg.setCancelable(false)    //다이얼로그의 바깥 화면을 눌렀을 때 다이얼로그가 닫히지 않도록 함
         dlg?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT)) //색 투명하게
 
-        if(img == 0){
-            dlg.iv_icon.visibility = View.GONE
-        }
-        else {
-            dlg.iv_icon.setImageResource(img)
-        }
         dlg.tv_content.text = content
         dlg.btn_ok.text = okText
         dlg.btn_cancel.text = cancelText
