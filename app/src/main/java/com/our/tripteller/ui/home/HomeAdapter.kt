d8be9d519bf94358108e9d5e0ba4c8d9d2d5f6bc
Yunhyunjo <gotqlc147@likelion.org>
2021-02-20 18:57:37 +0900

    filter, home, homeDetail

--- app/src/main/java/com/our/tripteller/ui/home/HomeAdapter.kt
+++ app/src/main/java/com/our/tripteller/ui/home/HomeAdapter.kt
@@ -31,10 +31,12 @@ class HomeViewHolder(itemView: View, val itemClick: (HomeData, View) -> Unit) : 
 
     var image : ImageView = itemView.findViewById(R.id.iv_mainimg)
     var region : TextView = itemView.findViewById(R.id.tv_region)
-    var intro : TextView = itemView.findViewById(R.id.tv_intro)
+    var intro1 : TextView = itemView.findViewById(R.id.tv_intro1)
+    var intro2 : TextView = itemView.findViewById(R.id.tv_intro2)
     var tag1 : TextView = itemView.findViewById(R.id.tv_tag1)
     var tag2 : TextView = itemView.findViewById(R.id.tv_tag2)
     var tag3 : TextView = itemView.findViewById(R.id.tv_tag3)
+    var tagTemp : TextView = itemView.findViewById(R.id.tv_tag_temp)
     var tag4 : TextView = itemView.findViewById(R.id.tv_tag4)
     var tag5 : TextView = itemView.findViewById(R.id.tv_tag5)
     var tag6 : TextView = itemView.findViewById(R.id.tv_tag6)
@@ -42,13 +44,47 @@ class HomeViewHolder(itemView: View, val itemClick: (HomeData, View) -> Unit) : 
     fun bind(myData: HomeData){
         Glide.with(itemView).load(myData.mainimg).into(image)
         region.text = myData.region
-        intro.text = myData.intro
-        tag1.text = myData.tag1
-        tag2.text = myData.tag2
-        tag3.text = myData.tag3
-        tag4.visibility = View.GONE
-        tag5.visibility = View.GONE
-        tag6.visibility = View.GONE
+        var top = myData.intro1.length
+        var bottom = myData.intro2.length
+        var cnt = myData.cnt
+
+        if(top <= bottom){
+            intro1.setBackgroundResource(R.drawable.home_intro_1_2)
+            intro2.setBackgroundResource(R.drawable.home_intro_2_2)
+        }
+        intro1.text = myData.intro1
+        intro2.text = myData.intro2
+        if(myData.tag1 == null)
+            tag1.visibility = View.GONE
+        else
+            tag1.text = "#" + myData.tag1
+        if(myData.tag2 == null)
+            tag2.visibility = View.GONE
+        else
+            tag2.text = "#" +myData.tag2
+        if(myData.tag3 == null)
+            tag3.visibility = View.GONE
+        else
+            tag3.text = "#" +myData.tag3
+        if(cnt == 4){
+            tagTemp.text = "#" +myData.tag4
+            tagTemp.visibility = View.VISIBLE
+            tag4.visibility = View.GONE
+        }
+        else {
+            if (myData.tag4 == null)
+                tag4.visibility = View.GONE
+            else
+                tag4.text = "#" + myData.tag4
+        }
+        if(myData.tag5 == null)
+            tag5.visibility = View.GONE
+        else
+            tag5.text = "#" +myData.tag5
+        if(myData.tag6 == null)
+            tag6.visibility = View.GONE
+        else
+            tag6.text = "#" +myData.tag6
         itemView.setOnClickListener{itemClick(myData, itemView)}
     }
 }
